9ba76858faa25a2313291ecfb5258145a394d4bc
summer <summer@summer-PC>
2017-02-22 23:15:00 +0800

    set的基本操作的封装与练习

---
+++ src/main/java/Test.java
@@ -0,0 +1,35 @@
+import java.util.Date;
+import java.util.List;
+
+import com.xu.redis.client.RedisClient;
+import com.xu.redis.model.User;
+
+public class Test {
+     
+	public static void main(String[] args) throws InterruptedException {
+		User user1 = new User();
+		user1.setEmail("sdjwejkwefjewbf");
+		user1.setLoginName("loginName");
+		user1.setMakeTime(new Date());
+		user1.setName("徐国强");
+		RedisClient.setObject("time", user1, 600);
+		Thread.sleep(2000l);
+		System.out.println(RedisClient.getRemainTimeByKey("time"));
+		
+		for(int i = 0;i<=100;i++){
+			User user2 = new User();
+			user2.setEmail("sdjwejkwefjewbf");
+			user2.setLoginName("loginName111"+i);
+			user2.setMakeTime(new Date());
+			user2.setName("徐国强");
+			Long address = RedisClient.setIntoListByLpush("test1", user2);
+			System.out.println("push的位置为:"+address);
+		}
+		
+		List<User>  list  = RedisClient.getRangeListByKey("test1", 0l, -1l, User.class);
+		for(User user:list){
+			System.out.println(user.getLoginName());
+		}
+	}
+
+}
--- src/main/java/com/xu/redis/client/RedisClient.java
+++ src/main/java/com/xu/redis/client/RedisClient.java
@@ -1,11 +1,11 @@
 package com.xu.redis.client;
 
-import java.awt.geom.FlatteningPathIterator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import org.springframework.aop.target.PoolingConfig;
-
 import com.alibaba.fastjson.JSON;
+import com.xu.redis.model.User;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -14,165 +14,248 @@ import redis.clients.jedis.JedisPoolConfig;
 /**
  * 
  * <p>
- * 	Redis客户端访问
+ * Redis客户端访问
  * </p>
  * 
- * @author xuguoqiang
- * @创建时间：2017年2月11日
+ * @author xuguoqiang 
+ * @创建时间：2017年2月11日 
  * @version： V1.0
  */
 public class RedisClient {
-	public  static  JedisPool jedisPool; // 池化管理jedis链接池
-static {
-		
-		//读取相关的配置
+	public static JedisPool jedisPool; // 池化管理jedis链接池
+
+	static {
+
+		// 读取相关的配置
 		ResourceBundle resourceBundle = ResourceBundle.getBundle("redis");
 		int maxActive = Integer.parseInt(resourceBundle.getString("redis.pool.maxActive"));
 		int maxIdle = Integer.parseInt(resourceBundle.getString("redis.pool.maxIdle"));
 		int maxWait = Integer.parseInt(resourceBundle.getString("redis.pool.maxWait"));
-		
-		String ip = resourceBundle.getString("redis.host");//本地redis服务器IP
+
+		String ip = resourceBundle.getString("redis.host");// 本地redis服务器IP
 		int port = Integer.parseInt(resourceBundle.getString("redis.port"));
-		
-		JedisPoolConfig config = new JedisPoolConfig();  
-		//设置最大连接数
+
+		JedisPoolConfig config = new JedisPoolConfig();
+		// 设置最大连接数
 		config.setMaxTotal(maxActive);
-		//设置最大空闲数
+		// 设置最大空闲数
 		config.setMaxIdle(maxIdle);
-		//设置超时时间
+		// 设置超时时间
 		config.setMaxWaitMillis(maxWait);
-		
-		//初始化连接池
-		jedisPool = new JedisPool(config, ip, port); 
+
+		// 初始化连接池
+		jedisPool = new JedisPool(config, ip, port);
 	}
 
+	/**
+	 * 向缓存中设置字符串内容
+	 * 
+	 * @param key
+	 *            key
+	 * @param value
+	 *            value
+	 * @return
+	 * @throws Exception
+	 */
+	public static boolean setString(String key, String value) throws Exception {
+		Jedis jedis = null;
+		try {
+			jedis = jedisPool.getResource();
+			jedis.set(key, value);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			jedisPool.returnResource(jedis);
+		}
+	}
 
-/**
- * 向缓存中设置字符串内容
- * @param key key
- * @param value value
- * @return
- * @throws Exception
- */
-public static boolean  setString(String key,String value) throws Exception{
-	Jedis jedis = null;
-	try {
-		jedis = jedisPool.getResource();
-		jedis.set(key, value);
+	/**
+	 * 根据key 获取内容String字符串
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static Object getString(String key) {
+		Jedis jedis = null;
+		try {
+			jedis = jedisPool.getResource();
+			Object value = jedis.get(key);
+			return value;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			jedisPool.returnResource(jedis);
+		}
+	}
+
+	/**
+	 * 删除缓存中得对象，根据key
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static boolean delString(String key) {
+		Jedis jedis = null;
+		try {
+			jedis = jedisPool.getResource();
+			jedis.del(key);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			jedisPool.returnResource(jedis);
+		}
+	}
+
+	/**
+	 * 向缓存中设置对象
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public static boolean setObject(String key, Object value) {
+		Jedis jedis = null;
+		try {
+			String objectJson = JSON.toJSONString(value);
+			jedis = jedisPool.getResource();
+			jedis.set(key, objectJson);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			jedisPool.returnResource(jedis);
+		}
+	}
+
+	/**
+	 * 从缓存里面取对象
+	 * 
+	 * @param key
+	 * @param value==object
+	 * @return
+	 * 
+	 */
+
+	public static <T> T getObject(String key, Class<T> clazz) {
+		Jedis jedis = null;
+		try {
+
+			jedis = jedisPool.getResource();
+			String value = jedis.get(key);
+			return JSON.parseObject(value, clazz);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			jedisPool.returnResource(jedis);
+		}
+	}
+
+	/**
+	 * 设置对象缓存失效时间 expireTime 失效时间=seconds
+	 */
+
+	public static boolean setObject(String key, Object object, int expireTime) {
+		Jedis jedis = null;
+		try {
+			String value = JSON.toJSONString(object);
+			jedis = jedisPool.getResource();
+			jedis.setex(key, expireTime, value);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 		return true;
-	} catch (Exception e) {
-		e.printStackTrace();
-		return false;
-	}finally{
-		jedisPool.returnResource(jedis);
 	}
-}
 
-/**
- * 根据key 获取内容String字符串
- * @param key
- * @return
- */
-public static Object getString(String key){
-	Jedis jedis = null;
-	try {
-		jedis = jedisPool.getResource();
-		Object value = jedis.get(key);
-		return value;
-	} catch (Exception e) {
-		e.printStackTrace();
-		return false;
-	}finally{
-		jedisPool.returnResource(jedis);
+	/**
+	 * 
+	 * 返回某个key剩余的时间，单位秒
+	 * 
+	 */
+
+	public static Long getRemainTimeByKey(String key) {
+		Jedis jedis = null;
+		try {
+			jedis = jedisPool.getResource();
+			return jedis.ttl(key);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-}
 
-/**
- * 删除缓存中得对象，根据key
- * @param key
- * @return
- */
-public static boolean delString(String key){
-	Jedis jedis = null;
-	try {
-		jedis = jedisPool.getResource();
-		jedis.del(key);
-		return true;
-	} catch (Exception e) {
-		e.printStackTrace();
-		return false;
-	}finally{
-		jedisPool.returnResource(jedis);
+	/*********************************** Set的使用 *****************************************************/
+
+	/**
+	 * 
+	 * 通过左边插入对象
+	 * 
+	 * @return 第几个元素
+	 * 
+	 */
+	public static Long setIntoListByLpush(String key, Object value) {
+		Jedis jedis = null;
+		try {
+			jedis = jedisPool.getResource();
+			String value1 = JSON.toJSONString(value);
+			return jedis.lpush(key, value1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-}
 
-
-/**
- * 向缓存中设置对象
- * @param key 
- * @param value
- * @return
- */
-public static boolean  setObject(String key,Object value){
-	Jedis jedis = null;
-	try {
-		String objectJson = JSON.toJSONString(value);
-		jedis = jedisPool.getResource();
-		jedis.set(key, objectJson);
-		return true;
-	} catch (Exception e) {
-		e.printStackTrace();
-		return false;
-	}finally{
-		jedisPool.returnResource(jedis);
+	/**
+	 * 
+	 * 通过右边插入对象
+	 * 
+	 * @return 第几个元素
+	 * 
+	 */
+	public static Long setIntoListByRpush(String key, Object value) {
+		Jedis jedis = null;
+		try {
+			jedis = jedisPool.getResource();
+			String value1 = JSON.toJSONString(value);
+			return jedis.rpush(key, value1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-}
 
-/**
- * 从缓存里面取对象
- * @param key 
- * @param value==object
- * @return
- * 
- * */
+	/**
+	 * 
+	 * 取出指定范围内的元素
+	 * 
+	 * @param <T>
+	 * @param <T>
+	 * @return List
+	 * 
+	 */
 
-public static <T> T getObject(String key,Class <T> clazz){
-	Jedis jedis = null;
-	try{
-		
-		jedis = jedisPool.getResource();
-		String value = jedis.get(key);
-		return JSON.parseObject(value, clazz);
-	}catch(Exception e){
-		e.printStackTrace();
-		return null;
-	}finally {
-	   jedisPool.returnResource(jedis);
+	@SuppressWarnings("unchecked")
+	public static <T> List<T> getRangeListByKey(String key, Long i, Long j, Class<User> clazz) {
+		Jedis jedis = null;
+		List<T> list = new ArrayList<T>();
+		try {
+			jedis = jedisPool.getResource();
+			List<String> resultList = jedis.lrange(key, i, j);
+			for (String result : resultList) {
+				list.add((T) JSON.parseObject(result, clazz));
+			}
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return list;
+		}
 	}
-}
-
-/**
- * 设置对象缓存失效时间
- * expireTime 失效时间=seconds
- * */
-
-
-
-public static boolean setObject(String key,Object object,int expireTime){
-	Jedis jedis = null;
-	try{
-		String value = JSON.toJSONString(object);
-		jedis = jedisPool.getResource();
-		jedis.setex(key, expireTime, value);
-	}catch(Exception e){
-		e.printStackTrace();
-		return false;
-	}
-	return true;
-}
-     
-
-
-
 
 }
